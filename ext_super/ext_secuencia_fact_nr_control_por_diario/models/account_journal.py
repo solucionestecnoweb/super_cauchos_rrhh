6da6bad6486bfc561eec4e91aa0047098b024706
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-02-12 15:49:19 -0400

    Modulo de nro de control y doc por diario corregido

--- ext_super/ext_secuencia_fact_nr_control_por_diario/models/account_journal.py
+++ ext_super/ext_secuencia_fact_nr_control_por_diario/models/account_journal.py
@@ -19,9 +19,16 @@ import xml.etree.ElementTree as ET
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
+    doc_sequence_id = fields.Many2one('ir.sequence', string='Secuencia Nro Documento',
+        help="Este campo asigna el nro a un documento sea factura, NC, ND", copy=False)
+    doc_sequence_number_next = fields.Char(compute='_compute_proximo_valor_doc')
+
     ctrl_sequence_id = fields.Many2one('ir.sequence', string='Secuencia Nro Control',
-        help="Este campo asigna el nro de control de un documento sea factura, NC, ND", required=False, copy=False)
+        help="Este campo asigna el nro de control de un documento sea factura, NC, ND", copy=False)
     ctrl_sequence_number_next = fields.Char(compute='_compute_proximo_valor')
+
+    def _compute_proximo_valor_doc(self):
+        self.doc_sequence_number_next=self.doc_sequence_id.number_next_actual
 
     def _compute_proximo_valor(self):
         self.ctrl_sequence_number_next=self.ctrl_sequence_id.number_next_actual
--- ext_super/ext_secuencia_fact_nr_control_por_diario/models/account_move.py
+++ ext_super/ext_secuencia_fact_nr_control_por_diario/models/account_move.py
@@ -48,29 +48,44 @@ class AccountMove(models.Model):
     
 
     def get_invoice_number_cli(self):
-        
+        '''metodo que crea el Nombre del asiento contable si la secuencia no esta creada, crea una con el
+        nombre: 'l10n_ve_cuenta_retencion_iva'''
         name=''
         if self.act_nota_entre==False:
-           name=self.name
+           name=self.get_invoice_number_unico()
         return name
 
     
     def get_refuld_number_cli(self):# nota de credito cliente
-       
+        '''metodo que crea el Nombre del asiento contable si la secuencia no esta creada, crea una con el
+        nombre: 'l10n_ve_cuenta_retencion_iva'''
         name=''
         if self.act_nota_entre==False:
-             name=self.name
+             name=self.get_invoice_number_unico()
         return name
 
     
     def get_refuld_number_pro(self): #nota de debito Cliente
-        
+        '''metodo que crea el Nombre del asiento contable si la secuencia no esta creada, crea una con el
+        nombre: 'l10n_ve_cuenta_retencion_iva'''
         name=''
         if self.act_nota_entre==False:
-             name=self.name
+             name=self.get_invoice_number_unico()
         return name
 
-
+    def get_invoice_number_unico(self):
+        name=''
+        if not self.journal_id.doc_sequence_id:
+            raise UserError(_('Este diario no tiene configurado el Nro de Documento. Vaya al diario, pestaña *Configuracion sec. Facturación* y en el campo *Proximo Nro Documento* agregue uno'))
+        else:
+            if not self.journal_id.doc_sequence_id.code:
+                raise UserError(_('La secuencia del Nro documento llamado * %s * de este diario, no tiene configurada el Código se secuencias')%self.journal_id.doc_sequence_id.name)
+            else:
+                SEQUENCE_CODE=self.journal_id.doc_sequence_id.code
+                company_id = self.company_id.id
+                IrSequence = self.env['ir.sequence'].with_context(force_company=company_id)
+                name = IrSequence.next_by_code(SEQUENCE_CODE)
+        return name
     
 
     def get_invoice_ctrl_number_unico(self):
@@ -79,37 +94,16 @@ class AccountMove(models.Model):
         name=''
         if self.act_nota_entre==False:
             #raise UserError(_('Ever %s')%self.company_id.confg_nro_control)
-            if self.company_id.confg_nro_control=='d':
-                if not self.journal_id.ctrl_sequence_id:
-                    raise UserError(_('Este diario no tiene configurado el Nro de control. vaya al diario y en el campo *Proximo Nro control* agregue uno'))
+            
+            if not self.journal_id.ctrl_sequence_id:
+                raise UserError(_('Este diario no tiene configurado el Nro de control. vaya al diario, pestaña *Configuracion sec. Facturación* y en el campo *Proximo Nro control* agregue uno'))
+            else:
+                if not self.journal_id.ctrl_sequence_id.code:
+                    raise UserError(_('La secuencia del Nro control llamado * %s * de este diario, no tiene configurada el Código se secuencias')%self.journal_id.ctrl_sequence_id.name)
                 else:
-                    if not self.journal_id.ctrl_sequence_id.code and self.journal_id.type=='sale':
-                        raise UserError(_('La secuencia del Nro control llamado * %s * de este diario, no tiene configurada el Código se secuencias')%self.journal_id.ctrl_sequence_id.name)
-                    else:
-                        SEQUENCE_CODE=self.journal_id.ctrl_sequence_id.code
-                        company_id = self.company_id.id
-                        IrSequence = self.env['ir.sequence'].with_context(force_company=company_id)
-                        name = IrSequence.next_by_code(SEQUENCE_CODE)
-
-            #--------------------------------#
-            if self.company_id.confg_nro_control=='c':
-                self.ensure_one()
-                SEQUENCE_CODE = 'l10n_ve_nro_control_unico_formato_libre'+str(self.company_id.id) #loca 14
-                company_id = self.company_id.id #loca 14
-                IrSequence = self.env['ir.sequence'].with_context(force_company=company_id) #loca 14
-                #raise UserError(_('IrSequence: %s')%IrSequence)
-                name = IrSequence.next_by_code(SEQUENCE_CODE)
-
-                # si aún no existe una secuencia para esta empresa, cree una
-                if not name:
-                    IrSequence.sudo().create({
-                        'prefix': '00-',
-                        'name': 'Localización Venezolana nro control Unico Factura Forma Libre %s' % 1,
-                        'code': SEQUENCE_CODE,
-                        'implementation': 'no_gap',
-                        'padding': 4,
-                        'number_increment': 1,
-                        'company_id': company_id, #loca 14
-                    })
+                    SEQUENCE_CODE=self.journal_id.ctrl_sequence_id.code
+                    company_id = self.company_id.id
+                    IrSequence = self.env['ir.sequence'].with_context(force_company=company_id)
                     name = IrSequence.next_by_code(SEQUENCE_CODE)
+
         return name
